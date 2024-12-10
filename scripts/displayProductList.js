75115c72405c80d1d08adb869932349097ddbc7f
matterattetatte <matias.marti@medieinstitutet.se>
2024-12-10 19:14:09 +0100

    FT - 23 styling, layout and formatting for product list view

--- scripts/displayProductList.js
+++ scripts/displayProductList.js
@@ -1,7 +1,8 @@
 import productContext from '../context/productContext.js';  // Import the context
 
-const productsDisplayedCount = 0
-const pageSize = 5; // only fetch products 5 each time
+
+let productsDisplayedCount = 0
+const pageSize = 6; // only fetch products 6 each time
 
 document.addEventListener("DOMContentLoaded", () => {
     const [showMoreBtn] = document.getElementsByClassName('show-more-products')
@@ -15,12 +16,21 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
         products.slice(productsDisplayedCount, productsDisplayedCount + pageSize).forEach((p) => {
-            const productItemWrapper = document.createElement('div')
+            const productItemWrapper = document.createElement('a')
+            productItemWrapper.href = "product-details?id=" + p._id
+            productItemWrapper.classList = "product-item-wrapper"
             productListWrapper.appendChild(productItemWrapper)
 
             const img = document.createElement('img')
             const name = document.createElement('p')
             const price = document.createElement('p')
+
+            img.src=p.image
+            img.classList="product-item-img"
+            name.textContent = p.name
+            name.classList="product-item-name"
+            price.textContent = '$' + p.price.$numberDecimal
+            price.classList="product-item-price"
             
             productItemWrapper.appendChild(img)
             productItemWrapper.appendChild(name)
