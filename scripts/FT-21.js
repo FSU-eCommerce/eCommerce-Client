7333fb58c7f96c24983e93ca59356b79fb368956
LEA_HASELSTEINER <leahaselsteiner@hotmail.com>
2024-12-11 00:12:29 +0100

    categories

--- scripts/FT-21.js
+++ scripts/FT-21.js
@@ -1,19 +1,32 @@
 document.addEventListener('DOMContentLoaded', () =>{
-    const productListWrapper = document.getElementById('product-latest');
-    const categoryToDisplay = queryParams.get('date');
+    const productLatestWrapper = document.getElementById('product-latest');
+
+    const categories = ['Women', 'Men', 'Formal', 'Casual', 'Dress'];
+
+    let products = [];//for fetched products
+
+// fetch? fetchProducts
 
 
-});
 // olika variabel
 
 const productItemWrapper = document.createElement('a');
 productItemWrapper.href =
 
 
+
+// 
+
+const displayLatestProducts = () => {
+    categories.forEach(categroy)
+}
+
+const productWrapper = document.createElement('div');
 const img = document.createElement('img');
 const name = document.createElement('p');
 const price = document.createElement('p');//maybe wont show
 const date = document.createElement('p');//maybe wont show
 
-// 
 
+
+});
