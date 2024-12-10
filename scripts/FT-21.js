811d95b3e9eee1ae4a485790aa5746ff818caf6e
LEA_HASELSTEINER <leahaselsteiner@hotmail.com>
2024-12-10 23:52:24 +0100

    variables

---
+++ scripts/FT-21.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    const productListWrapper = document.getElementById('product-latest');
+    const categoryToDisplay = queryParams.get('date');
+
+
+});
+// olika variabel
+
+const productItemWrapper = document.createElement('a');
+productItemWrapper.href =
+
+
+const img = document.createElement('img');
+const name = document.createElement('p');
+const price = document.createElement('p');//maybe wont show
+const date = document.createElement('p');//maybe wont show
+
+// 
+
