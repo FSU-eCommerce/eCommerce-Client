9aa079a0b958ee19b25043b093a6d7f7001980c3
PETER STENMAN <PETER.STENMAN@hotmail.se>
2024-12-11 20:14:49 +0100

    updated main with import of changeQuantity

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 // scripts/main.js
 import fetchProducts from './fetchProducts.js';  // Import the function to fetch products
 import { renderProducts } from './renderProducts.js'; // Import the rendering logic
-import { addToCart, renderCart } from './FT-7.js'; // Import the addToCart and renderCart functions
+import { addToCart, renderCart, changeQuantity } from './FT-7.js'; // Import the addToCart and renderCart functions
 
 document.addEventListener('DOMContentLoaded', async () => {
   // Fetch products before rendering
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   // Make addToCart globally available
   window.addToCart = addToCart;
+  window.changeQuantity = changeQuantity;
 
   // Now that products are fetched, render them
   renderProducts();
